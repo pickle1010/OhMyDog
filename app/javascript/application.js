37e7f45f78e74c8110e525176bd18b253b55b253
JosePernalete <josepernalete284@gmail.com>
2023-10-27 12:20:10 -0300

    Añadida gema de manejo de fecha

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+//= require flatpickr
+//= require flatpickr/plugins/confirmDate/confirmDate
+
+document.addEventListener('DOMContentLoaded', function() {
+    flatpickr('.your-selector', {
+      enableTime: true,
+      plugins: [
+        new confirmDatePlugin({})
+      ]
+    })
+  })
