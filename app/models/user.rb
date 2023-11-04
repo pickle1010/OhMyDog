f1d4d7a9276f4cae75c8307cc12be83aab8b608b
Lisandro Martinez <lisandromartz@gmail.com>
2023-11-04 19:37:17 -0300

    Add first_name, last_name, dni and address validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :timeoutable
+
+  validates :dni, :first_name, :last_name, :address, presence: true
+  validates :dni, uniqueness: true, numericality: { greater_than: 0 }
 end
