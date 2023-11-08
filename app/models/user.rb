50faa1bef2a08398fc82c6e62d0bf3e5494cfd95
Lisandro Martinez <lisandromartz@gmail.com>
2023-11-08 00:59:19 -0300

    Remove Sign Up and Cancel account functionalities

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :timeoutable
+  # :confirmable, :registerable, :lockable, :trackable and :omniauthable
+  devise :database_authenticatable, :recoverable, 
+        :rememberable, :validatable, :timeoutable
 
   validates :dni, :first_name, :last_name, :address, presence: true
   validates :dni, uniqueness: true, numericality: { greater_than: 0 }
