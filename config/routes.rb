778ecd4a0dab3e796d5f72e441450beea62b1282
Lisandro Martinez <lisandromartz@gmail.com>
2023-11-08 20:56:00 -0300

    Generate basic dog-related stuff using rails scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :dogs
   devise_for :users
   namespace :admin do
     resources :users
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_08_232745) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_08_234915) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,19 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_08_232745) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "dogs", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "breed"
+    t.string "color"
+    t.string "sex"
+    t.date "birthday"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_dogs_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -61,4 +74,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_08_232745) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "dogs", "users"
 end
