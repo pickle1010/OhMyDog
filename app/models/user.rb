8273df26741ae107f560344cf6b19241fe5068bf
Lisandro Martinez <lisandromartz@gmail.com>
2023-11-08 17:12:10 -0300

    Remove Remember_me functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :registerable, :lockable, :trackable and :omniauthable
+  # :confirmable, :registerable, :rememberable, :lockable, :trackable and :omniauthable
   devise :database_authenticatable, :recoverable, 
-        :rememberable, :validatable, :timeoutable
+         :validatable, :timeoutable
 
   validates :dni, :first_name, :last_name, :address, presence: true
   validates :dni, uniqueness: true, numericality: { greater_than: 0 }
