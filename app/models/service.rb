74cbcf9a955b594c562168dd7f52062926c0b565
FRT2000 <fernandoramireztolentino@hotmail.com>
2023-11-09 13:02:02 -0300

    validaciones y traducciones finales siguiendo historia de usuario

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,5 +1,4 @@
 class Service < ApplicationRecord
     validates :name , presence: true, uniqueness: true
     validates :name , format: { with: /\A[^0-9]+\z/, message: "solo permite ingresar letras"}
-    validates :price, presence: true, numericality: {only_float: true}
 end
