9f8451642ab0cb18fce1f38504fb6bdc242c96e3
FRT2000 <fernandoramireztolentino@hotmail.com>
2023-11-09 02:58:58 -0300

    agregando validaciones y traducciones para perro

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,4 +1,19 @@
 class Dog < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
+
+  validates :first_name, presence: true, format: {with: /\A[^0-9]+\z/, message: "solo permite ingresar letras"}    
+  validates :last_name, presence: true, format: {with: /\A[^0-9]+\z/, message: "solo permite ingresar letras"}    
+  validates :breed, presence: true, format: {with: /\A[^0-9]+\z/, message: "solo permite ingresar letras"}   
+  validates :color, presence: true, format: {with: /\A[^0-9]+\z/, message: "solo permite ingresar letras"}   
+  validates :sex, presence: true, format: {with: /\A[^0-9]+\z/, message: "solo permite ingresar letras"}   
+  validates :birthday, presence: true  
+  validate :photo_attached?
+
+  private
+
+  def photo_attached?
+    errors.add(:photo, "debe estar adjunta") unless photo.attached?
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -55,6 +55,14 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_08_234915) do
     t.index ["user_id"], name: "index_dogs_on_user_id"
   end
 
+  create_table "services", force: :cascade do |t|
+    t.string "name"
+    t.float "price"
+    t.boolean "active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
