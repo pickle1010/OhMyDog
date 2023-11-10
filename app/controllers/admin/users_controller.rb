22f3ef02f9f3ae5e01d57f646ca5875c34131c8f
Lisandro Martinez <lisandromartz@gmail.com>
2023-11-09 22:14:39 -0300

    Add button to see user dogs on users table and a select box to choose an existing owner when creating or updating a dog. Also display user_dni instead of user_id when showing a dog

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -22,13 +22,18 @@ class Admin::UsersController < ApplicationController
   def edit
   end
 
+  def show_dogs
+    @user = User.find(params[:id])
+    @dogs = @user.dogs
+  end  
+
   # POST /admin/users or /admin/users.json
   def create
     @user = User.new(user_params)
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to admin_user_url(@user), notice: "User was successfully created." }
+        format.html { redirect_to new_dog_path, notice: "User was successfully created." }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new, status: :unprocessable_entity }
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   namespace :admin do
-    resources :users
+    resources :users do
+      get 'show_dogs', on: :member
+    end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
