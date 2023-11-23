22fd6794dc0c3c69599da0727d7a6b1041dc3002
JosePernalete <josepernalete284@gmail.com>
2023-11-23 20:05:23 -0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -66,6 +66,13 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_23_045002) do
     t.index ["user_id"], name: "index_dogs_on_user_id"
   end
 
+  create_table "meetings", force: :cascade do |t|
+    t.date "start_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "name"
+  end
+
   create_table "services", force: :cascade do |t|
     t.string "name"
     t.float "price"
@@ -110,5 +117,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_23_045002) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "clinic_dogs", "dogs"
+  add_foreign_key "dogs", "users"
+  add_foreign_key "turn_forms", "dogs"
   add_foreign_key "turn_forms", "users"
 end
