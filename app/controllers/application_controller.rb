bb494c09f2a68d5fd7d510c57d50cdbbeb804c0c
Lisandro Martinez <lisandromartz@gmail.com>
2023-12-10 18:56:52 -0300

    Allow users to edit their own phone account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,6 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:dni, :first_name, :last_name, :address])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address, :phone])
     end
 end
